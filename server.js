30f8cf7848fd4b4b216d5eaf5313a242f14fbcd6
David <davidsima@windowslive.com>
2019-02-20 16:51:33 +0200

    More CORS tweaking

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const cors = (req, res, next) => {
 }
 app.options('/*', cors)
 app.get('/', (req, res) => {res.status(200).sendFile('readme.html', {root: __dirname})})
-app.post('/clarifai', clarifai.handleImage(db))
+app.post('/clarifai', cors, clarifai.handleImage(db))
 app.post('/register', register.handleRegister(db, bcrypt))
 app.post('/signin', signin.handleLogin(db, bcrypt))
 app.post('/entries', async (req, res) => {
